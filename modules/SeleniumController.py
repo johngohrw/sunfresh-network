1bd9dc103a21765ad48d48c447dd3ec673f3c29f
John Goh <johngohrw@gmail.com>
2020-04-13 15:01:34 +0800

    add dotenv file

--- modules/SeleniumController.py
+++ modules/SeleniumController.py
@@ -5,33 +5,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver import ActionChains
-from random import randint
 import time
 import datetime
+from random import randint
+import os
 from pyvirtualdisplay import Display
+from dotenv import load_dotenv
+load_dotenv()
 
 
-#===================================
 # VARIABLES
-#===================================
-
-chrome_driver_path = './drivers/chromedriver_macos'
+chrome_driver_path = './drivers/chromedriver_{}'.format(os.getenv('HOST_OS'))
 username_credentials = 'andyyap@sunfresh.my'
 password_credentials = 'Sunfresh2020'
 shopify_store_name = 'wefresh-by-webay-group'
 
-#===================================
-
 
 class SeleniumController():
     def __init__(self):
-        self.virtual_display = Display(visible=0, size=(800, 600))
-        self.virtual_display.start()
+        if os.getenv('HOST_OS') == 'linux':
+            self.virtual_display = Display(visible=0, size=(800, 600))
+            self.virtual_display.start()
         self.debug = True
         self.page_load_delay = 5
+        if self.debug: print("[Selenium] Loading webdriver from {}".format(chrome_driver_path))
         self.browser = webdriver.Chrome(executable_path=chrome_driver_path)
         self.browser.set_page_load_timeout(30)
-
         time.sleep(3)
 
         # isloggedin = False
@@ -43,7 +42,8 @@ class SeleniumController():
         self.browser.get('https://motherfuckingwebsite.com/')
         print(self.browser.page_source)
         self.browser.close()
-        self.virtual_display.stop()
+        if os.getenv('HOST_OS') == 'linux':
+            self.virtual_display.stop()
 
     def shopify_login(self):
         url = 'https://{}.myshopify.com/admin'.format(shopify_store_name)
