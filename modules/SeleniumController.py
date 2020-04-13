94463b2ff48b358f6356dfc81adfb41ec07bbec2
John Goh <johngohrw@gmail.com>
2020-04-13 13:54:11 +0800

    testing headless virtual display for ubuntu server

--- modules/SeleniumController.py
+++ modules/SeleniumController.py
@@ -8,12 +8,14 @@ from selenium.webdriver import ActionChains
 from random import randint
 import time
 import datetime
+from pyvirtualdisplay import Display
+
 
 #===================================
 # VARIABLES
 #===================================
 
-chrome_driver_path = './drivers/chromedriver_linux'
+chrome_driver_path = './drivers/chromedriver_macos'
 username_credentials = 'andyyap@sunfresh.my'
 password_credentials = 'Sunfresh2020'
 shopify_store_name = 'wefresh-by-webay-group'
@@ -23,17 +25,25 @@ shopify_store_name = 'wefresh-by-webay-group'
 
 class SeleniumController():
     def __init__(self):
+        self.virtual_display = Display(visible=0, size=(800, 600))
+        self.virtual_display.start()
         self.debug = True
         self.page_load_delay = 5
-        self.browser = webdriver.Chrome(chrome_driver_path)
+        self.browser = webdriver.Chrome(executable_path=chrome_driver_path)
         self.browser.set_page_load_timeout(30)
 
-        isloggedin = False
-        while not isloggedin:
-            isloggedin = self.shopify_login()
-            if not isloggedin:
-                print("not logged in.. trying again")
+        time.sleep(3)
 
+        # isloggedin = False
+        # while not isloggedin:
+        #     isloggedin = self.shopify_login()
+        #     if not isloggedin:
+        #         print("not logged in.. trying again")
+
+        self.browser.get('https://motherfuckingwebsite.com/')
+        print(self.browser.page_source)
+        self.browser.close()
+        self.virtual_display.stop()
 
     def shopify_login(self):
         url = 'https://{}.myshopify.com/admin'.format(shopify_store_name)
